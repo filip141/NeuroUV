95b86179851fc7c4050169c4ff2633c7280526f6
brzk4 <j.brzoza@yahoo.com>
2015-05-18 21:56:09 +0000

    controller added

--- navi.py
+++ navi.py
@@ -3,6 +3,7 @@
 import os
 import rospy
 from controlvehicle.msg import Maxsonar
+from controlvehicle.msg import joy_control_msg as Move
 
 print "start.."
 
@@ -26,44 +27,78 @@ class mapClass(object):
 	def displayMap(self):
 		os.system('clear')		
 
-		for x in range(self.szer):
-			print self.tab[x]
+		for y in reversed(range(self.dl)):
+			print self.tab[y]
 
 	def calc(self,msonar):
-		## przedni centralny
-		il_kratek = msonar.dwa/10
+		try:
+			## przedni centralny
+			il_kratek = msonar.dwa/10
 
-		self.tab[self.cent_szer+0][self.cent_dl+int(il_kratek)] = self.tab[self.cent_szer+0][self.cent_dl+int(il_kratek)] + 1	
-		self.tab[self.cent_szer+1][self.cent_dl+int(il_kratek)] = self.tab[self.cent_szer+1][self.cent_dl+int(il_kratek)] + 1	
-		self.tab[self.cent_szer-1][self.cent_dl+int(il_kratek)] = self.tab[self.cent_szer-1][self.cent_dl+int(il_kratek)] + 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer+0] += 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer+1] += 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer-1] += 1	
 
-		## przedni lewy
-		il_kratek2 = msonar.trzy/10
+			## przedni lewy
+			il_kratek = msonar.trzy/10
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer-5] += 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer-3] += 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer-4] += 1	
 
-		self.tab[self.cent_szer-2][self.cent_dl+int(il_kratek2)] = self.tab[self.cent_szer-2][self.cent_dl+int(il_kratek2)] + 1	
-		self.tab[self.cent_szer-3][self.cent_dl+int(il_kratek2)] = self.tab[self.cent_szer-3][self.cent_dl+int(il_kratek2)] + 1	
-		self.tab[self.cent_szer-4][self.cent_dl+int(il_kratek2)] = self.tab[self.cent_szer-4][self.cent_dl+int(il_kratek2)] + 1	
+			## przedni prawy
+			il_kratek = msonar.jeden/10
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer+5] += 1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer+3] +=  1	
+			self.tab[self.cent_dl+int(il_kratek)][self.cent_szer+4] +=  1	
 
-		## przedni prawy
-		il_kratek3 = msonar.jeden/10
+			## tylni
+			il_kratek = msonar.cztery/10
+			self.tab[self.cent_dl-int(il_kratek)][self.cent_szer+0] +=  1	
+			self.tab[self.cent_dl-int(il_kratek)][self.cent_szer+1] +=  1	
+			self.tab[self.cent_dl-int(il_kratek)][self.cent_szer-1] +=  1	
 
-		self.tab[self.cent_szer+2][self.cent_dl+int(il_kratek3)] = self.tab[self.cent_szer+2][self.cent_dl+int(il_kratek3)] + 1	
-		self.tab[self.cent_szer+3][self.cent_dl+int(il_kratek3)] = self.tab[self.cent_szer+3][self.cent_dl+int(il_kratek3)] + 1	
-		self.tab[self.cent_szer+4][self.cent_dl+int(il_kratek3)] = self.tab[self.cent_szer+4][self.cent_dl+int(il_kratek3)] + 1	
+		except IndexError:
+			pass
 
-		## tylni
-		il_kratek4 = msonar.cztery/10
-
-		self.tab[self.cent_szer+0][self.cent_dl-int(il_kratek4)] = self.tab[self.cent_szer+0][self.cent_dl-int(il_kratek4)] + 1	
-		self.tab[self.cent_szer+1][self.cent_dl-int(il_kratek4)] = self.tab[self.cent_szer+1][self.cent_dl-int(il_kratek4)] + 1	
-		self.tab[self.cent_szer-1][self.cent_dl-int(il_kratek4)] = self.tab[self.cent_szer-1][self.cent_dl-int(il_kratek4)] + 1	
-
-
-	def update(self):
-		pass
+	def refresh(self):
+		for x in range(self.szer):
+			for y in range(self.dl):
+				self.tab[x][y] = 0
 		
 
+	def initial_scan(self,msonar):
+		
+		self.refresh()
+		self.calc(msonar)
+		self.displayMap()
+		
+		try:
+			ControllerOBJ = Controller()
+			ControllerOBJ.turn90deg()
+		except rospy.ROSInterruptException:
+			pass
 
+		print "okrecilem sie"
+			
+class Controller(object):
+	## steruje robotem
+
+	def __init__(self):
+#		rospy.init_node('Controller',anonymous=True)
+		self.pub = rospy.Publisher('joyvehicle', Move, queue_size=1)
+		self.rate = rospy.Rate(10)
+		self.move = Move()
+	
+	def turn90deg(self):
+		self.move.sequence = 2
+		self.move.speed = 100
+		self.move.powerLR = 0
+	
+		self.pub.publish(self.move)
+		self.rate.sleep()		
+		
+		#rospy.spin()	
+		## close node
 
 class naviClass(object):
 	## sranie w banie
@@ -76,29 +111,57 @@ class naviClass(object):
 		self.trzy = 0
 		self.cztery = 0
 		self.map = map
+		self.move = 1
+		self.flag = True
+		self.is_started = False 	
 
 	def subscribeSonars(self):
 		rospy.Subscriber('maxsonar', Maxsonar, self.coefCallback)
+		rospy.Subscriber('joyvehicle', Move, self.moveCallBack)
 		rospy.spin()		
 
-	def coefCallback(self,msonar):
-		self.jeden = msonar.sonar1
-		self.dwa = msonar.sonar2 
-		self.trzy = msonar.sonar3
-		self.cztery = msonar.sonar4
-			
-		self.map.calc(self)
-		self.map.displayMap()
 
-	#	self.printData() ## drukuje wartosci  z sonar√w
+	def moveCallBack(self, move):
+		self.move = move.sequence
+
+	def coefCallback(self,msonar):
+	
+		## PIERWSZY SKAN 360 STOPNI
+		if not self.is_started:
+			self.map.initial_scan(self)
+			self.is_started = True		
+
+		"""## ALGORYTM SKANUJACY PO ZATRZYMANIU
+		print "1"
+		if self.move == 1:
+			if self.flag:
+				self.map.refresh()
+	
+			self.jeden = msonar.sonar1
+			self.dwa = msonar.sonar2 
+			self.trzy = msonar.sonar3
+			self.cztery = msonar.sonar4
+	
+			self.map.calc(self)
+			self.map.displayMap()
+	
+			self.flag = False
+		else:
+			self.flag = True
+
+	"""
+						
+		
+		
+		#self.printData() ## drukuje wartosci  z sonarow
 		
 	
 	def printData(self):
 		print "{}:{}:{}:{}\n------".format(self.jeden,self.dwa, self.trzy, self.cztery)
-
+		
 
 if __name__ == '__main__':
-	MapObj = mapClass(30,30)	
+	MapObj = mapClass(20,20)	
 	NaviObj = naviClass(MapObj)
 	NaviObj.subscribeSonars()
 	
